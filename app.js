b7549f5c8b871c3ea3813c4e597b8565556f50b7
Guttz <gustavopguto@gmail.com>
2018-02-10 17:38:36 -0200

    Server funcionando com login parcialmente implementado

---
+++ app.js
@@ -0,0 +1,76 @@
+
+/**
+	* Node.js Login Boilerplate
+	* More Info : http://kitchen.braitsch.io/building-a-login-system-in-node-js-and-mongodb/
+	* Copyright (c) 2013-2016 Stephen Braitsch
+**/
+
+var http = require('http');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+const path = require('path');
+var MongoStore = require('connect-mongo')(session);
+
+var app = express();
+
+app.locals.pretty = true;
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'jade');
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
+
+// build mongo database connection url //
+
+var dbHost = process.env.DB_HOST || 'localhost'
+var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'node-login';
+
+var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+if (app.get('env') == 'live'){
+// prepend url with authentication credentials // 
+	dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
+}
+
+app.use(session({
+	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+	proxy: true,
+	resave: true,
+	saveUninitialized: true,
+	store: new MongoStore({ url: dbURL })
+	})
+);
+
+require('./app/server/routes')(app);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
